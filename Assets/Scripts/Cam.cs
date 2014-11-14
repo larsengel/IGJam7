b53089dafffadac019fc1504601e43ca72353917
Dario D. Müller <dario-domi@hotmail.de>
2014-11-14 23:30:43 +0100

    Cam added

--- Assets/Scripts/Cam.cs
+++ Assets/Scripts/Cam.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class Cam : MonoBehaviour {
 
+	private float speed = -5.0f;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +12,8 @@ public class Cam : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+		this.transform.Rotate(new Vector3(0, 0, 1f * speed * Time.deltaTime));
+
 	}
 }
