82f399a7865b663f470a95b92c111d76f4537ee7
KallePower <kalle.muenster@gmx.de>
2014-11-14 23:25:55 +0100

    playerscript added

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	public int playerNumber;
+	public string playerName;
+
+	[SerializeField]
+	private float speed;
+
+	public enum DIRECTION
+	{LEFT,RIGHT}
+	private DIRECTION movingDirection;
+
+
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update() 
+	{
+	//	Input.GetAxis();
+	}
+}
