7727647416bd7a9035e19579a8a74b34f936eee0
larsengel <larsengel@ymail.com>
2014-11-15 22:54:04 +0100

    Changes before broken Project

--- Assets/Scripts/ItemSpawner.cs
+++ Assets/Scripts/ItemSpawner.cs
@@ -33,12 +33,10 @@ public class ItemSpawner : MonoBehaviour {
     {
 		float angle;
 		float rnd = Random.value;
-		Debug.Log (rnd);
 		if(rnd > 0.5f)
 			angle = Random.Range ((float)0.0,(float)0.25f) - 0.125f;
 		else
 			angle = Random.Range ((float)(0.375f), (float)(0.625f));
-		Debug.Log (angle);
 		angle *= 2 * Mathf.PI;
 
         float randomX = pLength * Mathf.Cos(angle) + GameMaster.Earth.transform.position.x;
