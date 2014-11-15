752df9881ff9c5478390ca7467f0d7fa03b07277
Dario D. Müller <dario-domi@hotmail.de>
2014-11-15 04:04:11 +0100

    Added functional Pickup Items

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -6,8 +6,9 @@ public class PlayerScript : MonoBehaviour {
 	public int playerNumber;
 	public string playerName;
 	private GlewGunScript gun;
-	public GameObject planet;
-	public GameObject catchObject=null;
+    private GameObject planet;
+	public Transform catchObject = null;
+    public bool catchFollowing = false;
 	[SerializeField]
 	private float speed;
 
@@ -18,7 +19,6 @@ public class PlayerScript : MonoBehaviour {
 		RIGHT = 1
 	}
 	private DIRECTION movingDirection;
-
 
 	void Start () 
 	{
@@ -37,24 +37,53 @@ public class PlayerScript : MonoBehaviour {
 
 	private void GetInputs()
 	{
-		if(playerNumber==1)
-		{
-			movingDirection=(DIRECTION)Input.GetAxis("PlayerAControll");
-		}
-		else
-		{
-			movingDirection=(DIRECTION)Input.GetAxis("PlayerBControll");
-		}
+        switch (playerNumber)
+        {
+            case 1:
+			    movingDirection=(DIRECTION)Input.GetAxis("PlayerAControll");
+                if (Input.GetKeyDown(KeyCode.Joystick2Button0))
+                    Catch();
+                break;
+            case 2:
+			    movingDirection=(DIRECTION)Input.GetAxis("PlayerBControll");
+                if (Input.GetKeyDown(KeyCode.Joystick1Button0))
+                    Catch();
+                break;
+        }
 	}
 
 	private void Move()
 	{
-		//move left or right...
-		this.transform.position += ( movingDirection == DIRECTION.LEFT ) ? -this.transform.right * speed : ( movingDirection == DIRECTION.RIGHT ) ? this.transform.right * speed : Vector3.zero;
-		//correct orientation angle...
-		this.gameObject.transform.up = -( planet.GetComponent<CircleCollider2D>().center - new Vector2(this.transform.position.x, this.transform.position.y) ).normalized;
-		//correct position (distance to planet-center)...
-		this.gameObject.transform.position += -transform.up * ( ( Vector2.Distance(this.planet.transform.position, new Vector2(this.transform.position.x, this.transform.position.y)) + this.transform.localScale.y ) - ( planet.GetComponent<CircleCollider2D>().radius * 2 - this.transform.localScale.y * 2 ) );
-	}
 
+        //move left or right...
+        Vector3 positionSide = (movingDirection == DIRECTION.LEFT) ? -this.transform.right * speed * 0.01f : (movingDirection == DIRECTION.RIGHT) ? this.transform.right * speed * 0.01f : Vector3.zero;
+        this.transform.position += positionSide;
+
+        //correct orientation angle...
+        Vector2 up = -(planet.GetComponent<CircleCollider2D>().center - new Vector2(this.transform.position.x, this.transform.position.y)).normalized;
+        this.gameObject.transform.up = up;
+
+        //correct position (distance to planet-center)...
+        Vector3 positionDown = -transform.up * ((Vector2.Distance(this.planet.transform.position, new Vector2(this.transform.position.x, this.transform.position.y)) + this.transform.localScale.y) - (planet.GetComponent<CircleCollider2D>().radius * 2 - this.transform.localScale.y * 2));
+        this.gameObject.transform.position += positionDown;
+
+        if (this.catchObject != null && catchFollowing == true)
+        {
+            this.catchObject.transform.position += positionSide;
+            this.catchObject.transform.up = up;
+            this.catchObject.transform.position += positionDown;
+        }
+    }
+
+    private void Catch()
+    {
+        if (this.catchObject != null && catchFollowing == false)
+        {
+            catchFollowing = true;
+        }
+        else if(catchFollowing == true)
+        {
+            catchFollowing = false;
+        }
+    }
 }
