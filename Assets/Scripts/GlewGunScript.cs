4e47e5d7d84b4a4f3852c8d79679e9a7fe360278
larsengel <larsengel@ymail.com>
2015-02-09 09:09:12 +0100

    Laser Status Bar

--- Assets/Scripts/GlewGunScript.cs
+++ Assets/Scripts/GlewGunScript.cs
@@ -8,19 +8,35 @@ public class GlewGunScript : MonoBehaviour
 	public static AudioSource fireSound;
     public float timeNeed = 1f;
     public float timeCurrent = 0;
+	public int playerNr;
     private Transform Spawn;
 
 	public bool cooldown = false;
+
+	public GameObject bar;
 
     void Start()
 	{
         Spawn = this.gameObject.transform.GetChild(0).transform;
 		fireSound = GameObject.Find("ScriptContainer/Laser").GetComponent<AudioSource>();
+		if(playerNr == 1)
+			bar = GameObject.Find("laser_red");
+		else
+			bar = GameObject.Find ("laser_blue");
 	}
 
     void Update()
     {
         timeCurrent = timeCurrent>timeNeed? timeNeed : timeCurrent + Time.deltaTime;
+		float sizex = bar.transform.localScale.x;
+		if (sizex < 1) {
+			sizex += 1*Time.deltaTime/timeNeed;
+		}
+		if (sizex > 1)
+			sizex = 1;
+
+		bar.transform.localScale = new Vector3(sizex, bar.transform.localScale.y, bar.transform.localScale.z);
+
     }
 
     public void Fire(PlayerScript.DIRECTION direction)
@@ -29,7 +45,7 @@ public class GlewGunScript : MonoBehaviour
 		{
             this.timeCurrent = 0;
 			fireSound.Play();
-
+			bar.transform.localScale = new Vector3(0, bar.transform.localScale.y, bar.transform.localScale.z);
 			timeNeed = 1f;
 			cooldown = false;
 
@@ -41,14 +57,15 @@ public class GlewGunScript : MonoBehaviour
 		{
 			if (cooldown == false)
 			{
-			this.timeCurrent = 0;
-			fireSound.Play();
+				this.timeCurrent = 0;
+				fireSound.Play();
+				bar.transform.localScale = new Vector3(0, bar.transform.localScale.y, bar.transform.localScale.z);
 
-			timeNeed = 2f;
-			cooldown = true;
-			
-			GameObject charge = GameObject.Instantiate(chargePrefab, Spawn.position, this.transform.rotation) as GameObject;
-			charge.GetComponent<GlueCharge>().movementDirection = direction;
+				timeNeed = 2f;
+				cooldown = true;
+				
+				GameObject charge = GameObject.Instantiate(chargePrefab, Spawn.position, this.transform.rotation) as GameObject;
+				charge.GetComponent<GlueCharge>().movementDirection = direction;
 			}
 		}
 	}
